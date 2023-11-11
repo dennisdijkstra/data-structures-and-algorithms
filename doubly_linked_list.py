7bbb1964225f180136126635caaf926f4eb72501
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-11 16:18:56 +0100

    Create new file

---
+++ doubly_linked_list.py
@@ -0,0 +1,38 @@
+class Node:
+  def __init__(self, value):
+    self.value = value 
+    self.next = None
+    self.prev = None
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.size = 0
+
+  def get(self, index):
+    pass
+
+  def insert_head(self, value):
+    pass
+
+  def insert_tail(self, value):
+    pass
+
+  def insert(self, value, index):
+    pass
+
+  def remove_head(self):
+    pass
+
+  def remove_tail(self):
+    pass
+
+  def remove(self, index):
+    pass
+
+  def update(self, index, value):
+    pass
+
+  def print(self):
+    pass
