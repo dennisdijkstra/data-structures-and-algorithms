65d61fadb0b988c51c3e0d47dfc101bfd84c967a
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-14 18:31:20 +0100

    Binary search tree class

---
+++ binary_search_tree.py
@@ -0,0 +1,36 @@
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+class BinarySearchTree:
+  def __init__(self):
+    self.root = None
+    
+  def insert(self):
+    pass
+  
+  def find(self):
+    pass
+  
+  def find_min(self):
+    pass
+  
+  def find_max(self):
+    pass
+  
+  def remove(self):
+    pass
+  
+  def contains(self):
+    pass
+
+  def pre_order(self):
+    pass
+
+  def in_order(self):
+    pass
+
+  def post_order(self):
+    pass
