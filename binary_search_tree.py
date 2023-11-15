fbb42e84a09fc9dec0cb3de12c95efb8483d6c4c
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-15 18:13:29 +0100

    BST insert and find

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -7,18 +7,66 @@ class Node:
 class BinarySearchTree:
   def __init__(self):
     self.root = None
+    self.size = 0
     
-  def insert(self):
-    pass
+  def insert(self, value):
+    new_node = Node(value)
+    
+    if self.root is None:
+      self.root = new_node
+      self.size += 1
+      return
+    
+    current_node = self.root
+    while current_node:
+      if value == current_node.value:
+        return
+      
+      if value < current_node.value:
+        if current_node.left:
+          current_node = current_node.left
+          continue
+        else:
+          current_node.left = new_node
+          self.size += 1
+          break
+      else:
+        if current_node.right:
+          current_node = current_node.right
+          continue
+        else:
+          current_node.right = new_node
+          self.size += 1
+          break  
   
-  def find(self):
-    pass
+  def find(self, value):
+    if self.root is None:
+      return None
+    
+    current_node = self.root
+    
+    while current_node:
+      if current_node.value == value:
+          return current_node
+        
+      if value < current_node.value:
+        current_node = current_node.left
+      else:
+        current_node = current_node.right
   
   def find_min(self):
-    pass
+    current_node = self.root
+    while current_node.left:
+        current_node = current_node.left
+
+    return current_node
   
   def find_max(self):
-    pass
+    current_node = self.root
+    while current_node.right:
+        current_node = current_node.right
+
+    return current_node
   
   def remove(self):
     pass
--- binary_search_tree_test.py
+++ binary_search_tree_test.py
@@ -1,1 +1,64 @@
 from binary_search_tree import BinarySearchTree
+
+def test_bst_init():
+  bst = BinarySearchTree()
+
+  assert bst.size == 0
+  assert bst.root == None
+
+def test_bst_insert():
+  bst = BinarySearchTree()
+  bst.insert(1)
+  bst.insert(20)
+  bst.insert(15)
+
+  assert bst.size == 3
+
+def test_bst_find_non_existing():
+  bst = BinarySearchTree()
+  values = [5, 33, 9, 16]
+
+  bst.insert(values[0])
+  bst.insert(values[1])
+  bst.insert(values[2])
+  found_node = bst.find(values[3])
+
+  assert found_node == None
+
+def test_bst_find():
+  bst = BinarySearchTree()
+  values = [5, 33, 9, 16]
+
+  bst.insert(values[0])
+  bst.insert(values[1])
+  bst.insert(values[2])
+  bst.insert(values[3])
+  found_node = bst.find(values[3])
+
+  assert found_node.value == values[3]
+
+def test_bst_find_min():
+  bst = BinarySearchTree()
+  values = [5, 33, 9, 16]
+
+  bst.insert(values[0])
+  bst.insert(values[1])
+  bst.insert(values[2])
+  bst.insert(values[3])
+  min_node = bst.find_min()
+
+  assert bst.size == 4
+  assert min_node.value == values[0]
+
+def test_bst_find_max():
+  bst = BinarySearchTree()
+  values = [45, 120, 87, 12] 
+
+  bst.insert(values[0])
+  bst.insert(values[1])
+  bst.insert(values[2])
+  bst.insert(values[3])
+  max_node = bst.find_max()
+
+  assert bst.size == 4
+  assert max_node.value == values[1]
