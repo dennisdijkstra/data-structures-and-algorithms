30e9075fe6558c423010bd8db402ba2d9d6a2005
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-18 16:22:12 +0100

    Add and remove nodes and edges

--- graph.py
+++ graph.py
@@ -2,18 +2,29 @@ class Graph:
   def __init__(self):
     self.graph = {}
     self.size = 0
-  
-  def add_vertex(self):
-    pass
-  
-  def remove_vertex(self):
-    pass
-    
-  def add_edge(self):
-    pass
-  
-  def remove_vertex(self):
-    pass
-  
-  def is_edge(self):
-    pass
+
+  def add_node(self, node):
+    if node not in self.graph:
+      self.graph[node] = []
+      self.size += 1
+
+  def remove_node(self, node):
+    if self.graph[node] is None:
+      return None
+
+    for key in self.graph:
+      self.remove_edge(key, node)
+
+    del self.graph[node]
+    self.size -= 1
+
+  def add_edge(self, from_node, to_node):
+    self.graph[from_node].append(to_node)
+    self.graph[to_node].append(from_node)
+
+  def remove_edge(self, from_node, to_node):
+    if to_node in self.graph[from_node]:
+      self.graph[from_node].remove(to_node)
+
+    if from_node in self.graph[to_node]:
+      self.graph[to_node].remove(from_node)
--- graph_test.py
+++ graph_test.py
@@ -3,5 +3,56 @@ from graph import Graph
 def test_graph_init():
   graph = Graph()
 
-  print(graph)
+  assert graph.size == 0
   assert graph.graph == {}
+
+def test_graph_add_node():
+  graph = Graph()
+  graph.add_node('A')
+  graph.add_node('B')
+
+  assert graph.size == 2
+
+def test_graph_add_edge():
+  graph = Graph()
+  node_a = 'A'
+  node_b = 'B'
+  node_c = 'C'
+
+  graph.add_node(node_a)
+  graph.add_node(node_b)
+  graph.add_node(node_c)
+  graph.add_edge(node_a, node_b)
+  graph.add_edge(node_a, node_c)
+
+  assert len(graph.graph[node_a]) == 2
+  assert len(graph.graph[node_b]) == 1
+  assert len(graph.graph[node_c]) == 1
+  
+def test_graph_remove_node():
+  graph = Graph()
+
+  node_a = 'A'
+  node_b = 'B'
+  node_c = 'C'
+  graph.add_node(node_a)
+  graph.add_node(node_b)
+  graph.add_node(node_c)
+  graph.add_edge(node_a, node_b)
+  graph.add_edge(node_a, node_c)
+  graph.remove_node(node_b)
+
+  assert len(graph.graph[node_a]) == 1
+  assert len(graph.graph[node_c]) == 1
+
+def test_graph_remove_edge():
+  graph = Graph()
+
+  node_a = 'A'
+  node_b = 'B'
+  graph.add_node(node_a)
+  graph.add_node(node_b)
+  graph.add_edge(node_a, node_b)
+  graph.remove_edge(node_a, node_b)
+
+  assert len(graph.graph[node_a]) == 0
