e14fcfc4d2a27133954abc43e7c47824cdb54b50
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-21 14:01:05 +0100

    Write tests for priority queue

--- priority_queue.py
+++ priority_queue.py
@@ -3,12 +3,15 @@ from max_heap import MaxHeap
 class PriorityQueue:
   def __init__(self):
     self.heap = MaxHeap(lambda a,b: a['priority'] > b['priority'])
+    self.size = 0
 
   def enqueue(self, value):
     self.heap.add(value)
+    self.size += 1
 
   def dequeue(self):
     self.heap.remove()
+    self.size -= 1
 
   def peek(self):
     return self.heap.peek()
