0a4c33ab5de6b462dd646a0e0c8267d212b05bda
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-11-22 15:16:57 +0100

    Implement trie

--- trie.py
+++ trie.py
@@ -7,15 +7,59 @@ class Trie:
   def __init__(self):
     self.root = Node()
     self.size = 0
-    
-  def insert(self, word):
-    pass
 
-  def remove(self):
-    pass
+  def insert(self, word):
+    node = self.root
+
+    for char in word:      
+      if not char in node.children:
+        node.children[char] = Node()
+
+      node = node.children[char]
+
+    node.is_end_of_word = True
+    self.size += 1
 
   def contains(self, word):
-    pass
+    node = self.root
+
+    for char in word:      
+      if not char in node.children:
+        return False
+
+      node = node.children[char]
+
+    return node.is_end_of_word
 
   def find(self, prefix):
-    pass
+    node = self.root
+
+    for char in prefix:
+      if not char in node.children:
+        return False
+
+      node = node.children[char]
+
+    return True
+
+  def remove(self, word):
+    self.delete(self.root, word, 0)
+
+  def delete(self, node, word, index):
+    if index == len(word):
+      if not node.is_end_of_word:
+        return False
+
+      del node.is_end_of_word
+      self.size -= 1
+      return True
+
+    char = word[index]
+    if not char in node.children:
+      return False
+
+    can_remove = self.delete(node.children[char], word, index + 1)
+    if can_remove:
+      del node.children[char]
+
+    return True
--- trie_test.py
+++ trie_test.py
@@ -2,6 +2,44 @@ from trie import Trie
 
 def test_trie_init():
   trie = Trie()
-  
+
   assert trie.size == 0
   assert bool(trie) == True
+
+def test_trie_insert():
+  trie = Trie()
+
+  trie.insert('hello')
+  trie.insert('world')
+  assert trie.size == 2
+
+def test_trie_contains():
+  trie = Trie()
+
+  trie.insert('hello')
+
+  assert trie.contains('hello') == True
+  assert trie.contains('world') == False
+
+  trie.insert('world')
+  assert trie.contains('world') == True
+
+def test_trie_find_prefix():
+  trie = Trie()
+
+  trie.insert('hello')
+  trie.insert('world')
+  print(trie.root.children['h'].children['e'].children)
+
+  assert trie.find('he') == True
+  assert trie.find('wor') == True
+
+def test_trie_remove():
+  trie = Trie()
+
+  trie.insert('hello')
+  trie.insert('world')
+  assert trie.contains('world') == True
+
+  trie.remove('world')
+  assert trie.contains('world') == False
