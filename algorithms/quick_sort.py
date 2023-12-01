ecbf2af939eb4904f973a16367b40f288b3bbb3c
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-12-01 15:45:38 +0100

    Recursive quick sort

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,19 @@
+def quick_sort(array, start_index, end_index):
+  if start_index < end_index:
+    pivot_index = partition(array, start_index, end_index)
+
+    quick_sort(array, start_index, pivot_index - 1)
+    quick_sort(array, pivot_index + 1, end_index)
+
+def partition(array, start_index, end_index):
+  pivot = array[end_index]
+  pivot_index = start_index
+
+  for i in range(start_index, end_index):
+    if array[i] <= pivot:
+      array[i], array[pivot_index] = array[pivot_index], array[i]
+      pivot_index += 1
+
+  array[pivot_index], array[end_index] = array[end_index], array[pivot_index]
+
+  return pivot_index
